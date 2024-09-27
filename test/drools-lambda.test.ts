15136255ae1956fb723c1279f70e32ba10a13804
Berend de Boer <berend@pobox.com>
2024-09-28 11:31:32 +1200

    Initial commit

---
+++ test/drools-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DroolsLambda from '../lib/drools-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/drools-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DroolsLambda.DroolsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
